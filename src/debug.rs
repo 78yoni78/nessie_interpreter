7cf72b3a2c426be80d34d536b7cf339b58d4d069
Yonatan Reicher <yony252525@gmail.com>
2021-01-14 12:55:19 +0200

    Updated disassembler

--- src/debug.rs
+++ src/debug.rs
@@ -5,12 +5,17 @@ pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
 
     let mut offset = 0;
     while offset < chunk.code.len() {
-        offset = disassemble_instruction(chunk, offset);
+        disassemble_instruction(chunk, &mut offset);
     }
 }
 
-fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
-    let opcode: OpCode = unsafe { ::std::mem::transmute(chunk.code[offset]) };
-    println!("{:0<4} {:?}", offset, opcode);
-    offset + 1
+fn disassemble_instruction(chunk: &Chunk, offset: &mut usize) {
+    let opcode: OpCode = unsafe { ::std::mem::transmute(chunk.code[*offset]) };
+    let instruction: &Instruction = unsafe { ::std::mem::transmute(&chunk.code[*offset]) };
+    *offset = *offset + opcode.inst_size();
+
+    //  we must be looking at an instruction inside of chunk.code
+    assert!(*offset < chunk.code.len());
+    
+    println!("{:0<4} {:?}", offset, *instruction);
 }
