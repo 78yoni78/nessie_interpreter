c48271e115c78fb0aa37fdfae6e5bd7bafb9a6c4
Yonatan Reicher <yony252525@gmail.com>
2021-01-14 19:38:26 +0200

    Fixed bug in disassembler

--- src/debug.rs
+++ src/debug.rs
@@ -12,10 +12,10 @@ pub fn disassemble_chunk(chunk: &Chunk, name: &str) {
 fn disassemble_instruction(chunk: &Chunk, offset: &mut usize) {
     let opcode: OpCode = unsafe { ::std::mem::transmute(chunk.code[*offset]) };
     let instruction: &Instruction = unsafe { ::std::mem::transmute(&chunk.code[*offset]) };
-    *offset = *offset + opcode.inst_size();
-
+    
     //  we must be looking at an instruction inside of chunk.code
     assert!(*offset < chunk.code.len());
     
     println!("{:0<4} {:?}", offset, *instruction);
+    *offset = *offset + opcode.inst_size();
 }
