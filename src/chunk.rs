b54f56317b3693c1d342652a721ba3f8646123be
Yonatan Reicher <yony252525@gmail.com>
2021-01-14 10:55:22 +0200

    Added add_constant

--- src/chunk.rs
+++ src/chunk.rs
@@ -11,3 +11,11 @@ pub struct Chunk{
     pub code: Vec<u8>, 
     pub constants: Vec<Value>,
 }
+
+impl Chunk {
+    /// Adds a new constant to the chunk and returns a reference to it
+    pub fn add_constant(&mut self, value: Value) -> &Value {
+        self.constants.push(value);
+        &self.constants.last().unwrap()
+    }
+}
